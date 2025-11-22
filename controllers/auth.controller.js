ff7435debd0ddd15c4d7dc463d7631c0013fdc5b
diadeor <85044959+diadeor@users.noreply.github.com>
2025-11-22 07:53:24 +0545

    Changes to cookies

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -27,7 +27,7 @@ export const googleLogin = async (req, res, next) => {
 
     res.cookie("token", appToken, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 30,
+      maxAge: 1000 * 60 * 60 * 24,
       secure: true,
       sameSite: "none",
     });
